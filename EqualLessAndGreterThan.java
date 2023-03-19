5ecb0c940e96a4369694c329a50f8a777fae71fe
kiran68 <kiranurshil88@gmail.com>
2023-03-19 20:06:36 +0530

    Add: Compare Two Line Length

---
+++ EqualLessAndGreterThan.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class EqualLessAndGreterThan {
+public static void main(String[] args) {
+	 String s1="Mahesh";  
+	   String s2="Yogesh";  
+	   String s3="Ravi";  
+	   System.out.println(s1.compareTo(s2));//0  
+	   System.out.println(s1.compareTo(s3));//1(because s1>s3)  
+	   System.out.println(s3.compareTo(s1));//-1(because s3 < s1 )  
+	   
+	
+}
+}
